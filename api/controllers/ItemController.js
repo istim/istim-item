d4eb1e88a8b51218e79add01c5ee389054b85f2d
Lucas Ferreira <lucasfercunha@gmail.com>
2014-03-08 00:25:14 -0300

    Listar itens por usuário

--- api/controllers/ItemController.js
+++ api/controllers/ItemController.js
@@ -1,8 +1,8 @@
 /**
- * SaleController
+ * ItemController
  *
  * @module      :: Controller
- * @description :: A set of functions called `actions`.
+ * @description	:: A set of functions called `actions`.
  *
  *                 Actions contain code telling Sails how to respond to a certain type of request.
  *                 (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
@@ -17,14 +17,34 @@
 
 module.exports = {
 
+    mine: function(req, res) {
 
+        if ( ! req.session.user_id) {
+            res.statusCode = 400;
+            return res.send({error: true, type: 'NotAuthorizedError', message: 'You are not authorized to access this page.'});
+        }
 
+        if (req.method != 'POST') {
+            res.statusCode = 400;
+            return res.send({error: true, type: 'BadRequestError', message: 'Invalid request method, must use POST.'});
+        }
 
-  /**
-   * Overrides for the settings in `config/controllers.js`
-   * (specific to SaleController)
-   */
-  _config: {}
+        var query    = ['SELECT',
+                        'item_id as id, i.name, i.price, i.image,',
+                        'u.createdAt, u.updatedAt',
+                        'FROM Item i',
+                        'JOIN UserItem u',
+                        'ON u.item_id = i.id',
+                        'WHERE u.user_id = ' + req.session.user_id,
+                        'ORDER BY u.createdAt ASC'].join(' ');
 
+        var callBack = function(err, items) {
+            if (err) res.send(err);
+            else res.send(items);
+        };
+
+        var items = Item.query(query, callBack);
+
+    }
 
 };
