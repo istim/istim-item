226f97854f4904d358975c36114fcec143ec028e
Lucas Ferreira <lucasfercunha@gmail.com>
2014-03-12 19:25:41 -0300

    Primeira suite de testes

--- tests/test.js
+++ tests/test.js
@@ -8,6 +8,7 @@
 var Sails    = require('sails');
 var assert   = require('assert');
 var request  = require('supertest');
+var should   = require('should');
 
 /**
  * Bootstrap the server
@@ -71,4 +72,152 @@ describe('UserController', function() {
             });
         });
     });
+});
+
+/**
+ * Test Cases: UserItemController
+ */
+describe('UserItemController', function() {
+    describe('#mine()', function() {
+        it('deve retornar erro caso seja feita uma requisição POST', function(done) {
+            request(app.express.app).post('/useritem/mine').expect(405).end(function(err, res) {
+                assert.ok(res.body.error);
+                assert.equal('MethodNotAllowedException', res.body.type);
+                done();
+            });
+        });
+        it('deve retornar erro caso o usuário não esteja logado', function(done) {
+            request(app.express.app).get('/useritem/mine').end(function(err, res) {
+                assert.ok(res.body.error);
+                assert.equal('UnauthorizedException', res.body.type);
+                done();
+            });
+        });
+        it('deve retornar uma lista de itens que o usuário possui', function(done) {
+            request(app.express.app).get('/useritem/mine?login=1').end(function(err, res) {
+                res.body.should.be.instanceOf(Array).and.should.not.be.empty;
+                done();
+            });
+        });
+        it('deve retornar uma lista vazio para um usuário sem itens', function(done) {
+            request(app.express.app).get('/useritem/mine?login=2').end(function(err, res) {
+                res.text.should.be.equal('[]');
+                done();
+            });
+        });
+    });
+});
+
+/**
+ * Test Cases: SaleController
+ */
+describe('SaleController', function() {
+    describe('#for_sale()', function() {
+        it('deve retornar erro caso seja feita uma requisição POST', function (done) {
+            request(app.express.app).post('/sale/for_sale').expect(405).end(function(err, res) {
+                assert.ok(res.body.error);
+                assert.equal('MethodNotAllowedException', res.body.type);
+                done();
+            });
+        });
+        it('deve retornar erro caso o usuário não esteja logado', function(done) {
+            request(app.express.app).get('/sale/for_sale').end(function(err, res) {
+                assert.ok(res.body.error);
+                assert.equal('UnauthorizedException', res.body.type);
+                done();
+            });
+        });
+        it('deve retornar uma lista vazia para usuário que não pode comprar nada', function(done) {
+            request(app.express.app).get('/sale/for_sale?login=1').end(function(err, res) {
+                res.text.should.be.equal('[]');
+                done();
+            });
+        });
+        it('deve retornar uma lista de itens a venda quando o usuário pode comprar', function(done) {
+            request(app.express.app).get('/sale/for_sale?login=2').end(function(err, res) {
+                res.body.should.be.instanceOf(Array);
+                done();
+            });
+        });
+    });
+    describe('#selling()', function() {
+        it('deve retornar erro caso seja feita uma requisição POST', function (done) {
+            request(app.express.app).post('/sale/selling').expect(405).end(function(err, res) {
+                assert.ok(res.body.error);
+                assert.equal('MethodNotAllowedException', res.body.type);
+                done();
+            });
+        });
+        it('deve retornar erro caso o usuário não esteja logado', function(done) {
+            request(app.express.app).get('/sale/selling').end(function(err, res) {
+                assert.ok(res.body.error);
+                assert.equal('UnauthorizedException', res.body.type);
+                done();
+            });
+        });
+        it('deve retornar a lista de itens que o usuário está vendendo', function(done) {
+            request(app.express.app).get('/sale/selling?login=1').end(function(err, res) {
+                res.body.should.be.instanceOf(Array);
+                done();
+            });
+        });
+        it('deve retornar uma lista vazio para quem não está vendendo nada', function(done) {
+            request(app.express.app).get('/sale/selling?login=2').end(function(err, res) {
+                res.text.should.be.equal('[]');
+                done();
+            });
+        });
+    });
+    describe('#buy()', function() {
+        it('deve retornar erro caso seja feita uma requisição GET', function (done) {
+            request(app.express.app).get('/sale/buy').expect(405).end(function(err, res) {
+                assert.ok(res.body.error);
+                assert.equal('MethodNotAllowedException', res.body.type);
+                done();
+            });
+        });
+        it('deve retornar erro caso o usuário não esteja logado', function(done) {
+            request(app.express.app).post('/sale/buy').end(function(err, res) {
+                assert.ok(res.body.error);
+                assert.equal('UnauthorizedException', res.body.type);
+                done();
+            });
+        });
+        it('deve retornar erro caso o usuário não especifique qual a compra', function(done) {
+            request(app.express.app).post('/sale/buy?login=2').end(function(err, res) {
+                assert.ok(res.body.error);
+                assert.equal('MissingMandatoryParametersException', res.body.type);
+                done();
+            });
+        });
+        it('deve retornar erro caso não exista o item procurado', function(done) {
+            request(app.express.app).post('/sale/buy?login=2').send({sale_id:394}).end(function(err, res) {
+                assert.ok(res.body.error);
+                assert.equal('InGameGenericError', res.body.type);
+                res.body.message.should.containEql('not found');
+                done();
+            });
+        });
+        it('deve retornar erro caso o item já tenha sido comprado', function(done) {
+            request(app.express.app).post('/sale/buy?login=2').send({sale_id:4}).end(function(err, res) {
+                assert.ok(res.body.error);
+                assert.equal('InGameGenericError', res.body.type);
+                res.body.message.should.containEql('already been sold');
+                done();
+            });
+        });
+        // it('deve retornar uma lista vazio para quem não está vendendo nada', function(done) {
+        //     request(app.express.app).get('/sale/buy?login=2').end(function(err, res) {
+        //         res.text.should.be.equal('[]');
+        //         done();
+        //     });
+        // });
+    });
+});
+
+/**
+ * Lower sails
+ */
+after(function(done) {
+    app.lower(done);
 });
