6e7d65d08f1c8c89ee298e975c968292cc353f2d
Lucas Ferreira <lucasfercunha@gmail.com>
2014-03-13 14:01:05 -0300

    Configurando o travis

--- run_test.sh
+++ run_test.sh
@@ -1,3 +1,3 @@
-mysql --user="root" --password="" < tests/tables.sql
-mysql --user="root" --password="" < tests/rows.sql
+mysql -e < tests/tables.sql
+mysql -e < tests/rows.sql
 mocha tests/test.js --reporter spec --colors --recursive
